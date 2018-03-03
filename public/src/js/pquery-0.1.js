f3853f83ab8696a63c0413acb18825eb454f19b3
Joseph Sapp <sapp.joey@tamu.edu>
2018-03-03 16:40:20 -0600

    framerates

--- public/src/js/pquery-0.1.js
+++ public/src/js/pquery-0.1.js
@@ -1,17 +1,24 @@
-var canvas, cam;
+var globe, cam;
+var out;
 
 function setup(){
-	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-	canvas.parent('holder');
+	globe = createCanvas(windowWidth, windowHeight, WEBGL);
+	out = createDiv(windowWidth/8, windowHeight);
+	globe.parent('holder');
+	globe.style("position: absolute; top: 0px;");
+	out.parent('holder');
+	out.style("position: absolute; top: 0px;");
 
 	cam = createEasyCam();
 }
 
 function draw(){
 	background(255);
+	fill(255);
 	torus(50, 25);
+	out.html(String(frameRate()));
 }
 
 function windowResized(){
-	canvas.size(windowWidth, windowHeight);
+	globe.size(windowWidth, windowHeight);
 }
