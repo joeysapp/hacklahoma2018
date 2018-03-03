8e39cb652d974e8a1261b909dad097f0f112c83c
Joseph Sapp <sapp.joey@tamu.edu>
2018-03-03 16:03:35 -0600

    Working camera and floating donut

--- public/src/js/pquery-0.1.js
+++ public/src/js/pquery-0.1.js
@@ -1,22 +1,17 @@
-var canvas;
+var canvas, cam;
 
 function setup(){
 	canvas = createCanvas(windowWidth, windowHeight, WEBGL);
 	canvas.parent('holder');
-	console.log('hi');
+
+	cam = createEasyCam();
 }
 
-var rx  = 0;
-var ry = 0;
-var rz =0 ;
-
 function draw(){
-	canvas.background(255);
-	canvas.rotateX(rx += 0.01);
-	canvas.rotateY(ry += 0.01);
-	canvas.rotateZ(rz += 0.01);
-	box(50);
+	background(255);
+	torus(50, 25);
+}
 
-
-
+function windowResized(){
+	canvas.size(windowWidth, windowHeight);
 }
